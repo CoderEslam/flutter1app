cd0ea515aa5a7d127d3e141d111d3a646f43698e
eslam <eslamfgazy600@gmail.com>
2022-11-27 02:06:42 +0200

    learn Scaffold

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,29 @@
 import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(
-  home: Text('hello EslamGhazy'),
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('my  first app'),
+          centerTitle: true, // to pur title in center
+        ),
+        body: const Center(
+          child: Text('Helloo Eslam Ghazy'),
+        ),
+        bottomNavigationBar: BottomAppBar(
+          shape: const CircularNotchedRectangle(),
+          child: Container(height: 50.0),
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            // Add your onPressed code here!
+          },
+          backgroundColor: Colors.green,
+          child: const Icon(Icons.navigation), // Text('click')
+        ),
+        floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      ),
+    ));
 
-));
+int f() {
+  return 5;
+}
