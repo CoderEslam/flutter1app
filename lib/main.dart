4f08c729272edab9ae9706d40319a8c354f8fe90
eslam <eslamfgazy600@gmail.com>
2022-11-27 02:50:58 +0200

    learn how to change color and font

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 
 void main() => runApp(MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: const Text('my  first app'),
           centerTitle: true, // to pur title in center
+          backgroundColor: Colors.yellow,
         ),
         body: const Center(
-          child: Text('Helloo Eslam Ghazy'),
+          child: Text(
+            'Hello Eslam Ghazy',
+            style: TextStyle(
+                fontSize: 22.0,
+                fontWeight: FontWeight.bold,
+                letterSpacing: 2.0,
+                color: Colors.grey,
+                fontFamily: 'india'),
+          ),
         ),
         bottomNavigationBar: BottomAppBar(
           shape: const CircularNotchedRectangle(),
@@ -16,11 +26,18 @@ void main() => runApp(MaterialApp(
         floatingActionButton: FloatingActionButton(
           onPressed: () {
             // Add your onPressed code here!
+            Fluttertoast.showToast(
+                msg: "This is a Toast message",
+                toastLength: Toast.LENGTH_SHORT,
+                gravity: ToastGravity.CENTER,
+                timeInSecForIosWeb: 1,
+                textColor: Colors.white,
+                fontSize: 16.0);
           },
-          backgroundColor: Colors.green,
+          backgroundColor: Colors.yellow,
           child: const Icon(Icons.navigation), // Text('click')
         ),
-        floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+        floatingActionButtonLocation: FloatingActionButtonLocation.endDocked,
       ),
     ));
 
